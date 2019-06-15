7021ac37c76ab1efc9eefee0943c6e2631ca8962
Oscar Gonzalez Lopez <oscar.gonzalez@cern.ch>
2019-06-15 17:13:18 +0200

    Adding the OglezDTAB7RawToDigi to the EventFilter/DTRawToDigi package of CMSSW

--- EventFilter/DTRawToDigi/plugins/SealModule.cc
+++ EventFilter/DTRawToDigi/plugins/SealModule.cc
@@ -2,7 +2,9 @@
 
 #include <EventFilter/DTRawToDigi/plugins/DTUnpackingModule.h>
 #include <EventFilter/DTRawToDigi/plugins/DTDigiToRawModule.h>
+#include <EventFilter/DTRawToDigi/interface/OglezDTAB7RawToDigi.h>
 
 
 DEFINE_FWK_MODULE(DTUnpackingModule);
 DEFINE_FWK_MODULE(DTDigiToRawModule);
+DEFINE_FWK_MODULE(OglezDTAB7RawToDigi);
