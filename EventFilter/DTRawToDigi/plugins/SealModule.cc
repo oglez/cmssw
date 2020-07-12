780c9de194579b7b069007bcbb0b25ca88b7c136
Oscar Gonzalez Lopez <oscar.gonzalez@cern.ch>
2020-07-12 08:53:01 +0200

    Trying to fix a conflict temporarily

--- EventFilter/DTRawToDigi/plugins/SealModule.cc
+++ EventFilter/DTRawToDigi/plugins/SealModule.cc
@@ -2,7 +2,6 @@
 
 #include <EventFilter/DTRawToDigi/plugins/DTUnpackingModule.h>
 #include <EventFilter/DTRawToDigi/plugins/DTDigiToRawModule.h>
-#include <EventFilter/DTRawToDigi/interface/OglezDTAB7RawToDigi.h>
 
 DEFINE_FWK_MODULE(DTUnpackingModule);
 DEFINE_FWK_MODULE(DTDigiToRawModule);
