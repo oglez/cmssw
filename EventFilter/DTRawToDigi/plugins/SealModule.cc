5a2234d0391ee98a3ffa5e2f3e20d25a7f52a646
Oscar Gonzalez Lopez <oscar.gonzalez@cern.ch>
2021-02-17 16:42:20 +0100

    Fixing the missing line in SealModule.cc for the DT Unpacker for Phase 2

--- EventFilter/DTRawToDigi/plugins/SealModule.cc
+++ EventFilter/DTRawToDigi/plugins/SealModule.cc
@@ -5,3 +5,6 @@
 
 DEFINE_FWK_MODULE(DTUnpackingModule);
 DEFINE_FWK_MODULE(DTDigiToRawModule);
+
+#include "EventFilter/DTRawToDigi/interface/OglezDTAB7RawToDigi.h"
+DEFINE_FWK_MODULE(OglezDTAB7RawToDigi);
